c5ba2d3199232b3d630d72c37ba651060d98eef5
ivan <ghalut@mail.ru>
2022-03-07 11:11:18 +0300

    lesson-2

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-
 export function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Hello, {props.name}</h3>
+        <h3>Lesson 2, {props.name}</h3>
       </header>
     </div>
   );
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 const myName = 'ReactJS';
-const message = 'Hello';
+const message = 'Lesson 1';
 
+const FunctionComponent = ({onClick}) =>{
+  const [messages, setMessages] = useState(["Message 1"])
+  const sendMessage = (message) => {
+setMessages([...messages, message])
+  }
+  
+  // useEffect lesson-2
+  useEffect(() => {
+    const TEXT = "hello from bot"
+    
+    const lastMessages = messages[messages.length - 1]
+    let timerId = null
+    if(lastMessages !== TEXT){
+    timerId = setTimeout(() => {
+        setMessages([...messages, TEXT])
+      },1000)
+    }
+    return () => {
+      // alert("useEffect")
+      clearInterval(timerId)
+    }
+  }, [messages])
+  
+  // -------
+  return (
+<div>
+<button class = "btnBot" onClick={() => sendMessage(<h1>test</h1>)}>sendMessage</button>
+  <h2>{messages.map(m => <h3 class="botAnswer">{m}</h3>)}</h2>
+</div>
+  )
+}
+
+// ===============
 const reactElement = (
   <div>
 <h2 className="App">Hello React</h2>
@@ -18,6 +51,10 @@ const Message = () => {
 }
 
 class ClassComponent extends React.Component {
+  // constructor(props){
+  //   alert('constructor')
+  //   super(props)
+  // }
   render() {
     const {onClick} = this.props;
     return (
@@ -28,14 +65,17 @@ class ClassComponent extends React.Component {
   }
 }
 
-
-
 ReactDOM.render(
   <React.StrictMode>
 <Message />
+<Example/>
+{/* <MyResponsiveComponent/> */}
+<Counter/>
+<ClassComponent onClick={(target)=>{alert(target)}}/>
 <hr/>
     <App name={myName} />
-    <ClassComponent onClick={(target)=>{alert(target)}}/>
+   
+  <FunctionComponent/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -44,3 +84,56 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+// function MyResponsiveComponent() {
+//   const width = useWindowWidth(); 
+//   return (
+//     <p>Window width is {width}</p>
+//   );
+// }
+// function useWindowWidth() {
+//   const [width, setWidth] = useState(window.innerWidth);
+  
+//   useEffect(() => {
+//     const handleResize = () => setWidth(window.innerWidth);
+//     window.addEventListener('resize', handleResize);
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   });
+  
+//   return width;
+// }
+
+function Counter({initialCount}) {
+  initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+  return (
+    <div class="counter">
+      Count: {count}
+      <button id="btn1" onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </div>
+  );
+}
+// --------------
