7f273f83a913aa89bca576926e47b940cb709b29
ivan <ghalut@mail.ru>
2022-04-07 12:19:25 +0300

    lesson6

--- myapp/src/components/layout/layout.js
+++ myapp/src/components/layout/layout.js
@@ -2,11 +2,13 @@ import styles from "./layout.module.css";
 
 export function Layout({ messages, chats }) {
   return (
+    <div className={styles.wrapper}>
     <div className={styles.body}>
       <div className={styles.content}>
         <div className={styles.chats}>{chats}</div>
         <div className={styles.messages}>{messages}</div>
       </div>
+    </div>
     </div>
   );
 }
