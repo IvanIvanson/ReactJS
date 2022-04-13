fca4c0bcaa29f0332d62eea816c798b9f99b85dc
ivan <ghalut@mail.ru>
2022-04-13 20:10:47 +0300

    lesson8

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,39 +1,33 @@
-import { combineReducers, createStore, applyMiddleware, compose } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import thunk from "redux-thunk";
-import { profileReducer } from "./profile";
-import { conversationsReducer } from "./conversations";
-import { messagesReducer } from "./messages";
-import {
-  logger,
-  botMessage,
-  timeScheduler,
-  crashReporter,
-} from "./middlewares";
-// import { createStore } from "./my-redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components";
+import { ProfilePage, ChatPage } from "./pages";
+import { CustomThemeProvider } from "./theme-context";
+import { store, persistor } from "./store";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["messages"],
-  whitelist: ["profile"],
-};
+import "./global.css";
 
-const reducer = combineReducers({
-  profile: profileReducer,
-  conversations: conversationsReducer,
-  messages: messagesReducer,
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <CustomThemeProvider>
+          <BrowserRouter>
+            <Header />
 
-export const store = createStore(
-  persistReducer(persistConfig, reducer),
-  compose(
-    applyMiddleware(crashReporter, thunk, logger, botMessage, timeScheduler),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (args) => args
-  )
+            <Routes>
+              <Route path="/" element={<h1>Home page</h1>} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/chat/*" element={<ChatPage />} />
+              <Route path="*" element={<h1>404 page</h1>} />
+            </Routes>
+          </BrowserRouter>
+        </CustomThemeProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-export const persistor = persistStore(store);
